10644aa4e354e3d8253ce935a97e749b46039a13
Tom Esparon <tespo@btinternet.com>
2023-03-05 19:05:27 +0000

    added comments

--- lib/question.dart
+++ lib/question.dart
@@ -3,4 +3,8 @@ class Question {
   bool questionAnswer;
 
   Question(this.questionText, this.questionAnswer);
+
+  //  NULL SAFETY TIP: If you want named parameters
+  // https://github.com/DetainedDeveloper/App-Brewery-Flutter-Null-Safety#section-10--quizzler-app-lesson-94
+  // Question({required this.questionText, required this.questionAnswer});
 }
