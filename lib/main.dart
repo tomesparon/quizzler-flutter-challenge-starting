a13a6a3e81d3d93e1c0cf9c1b563819e3d26cc3c
Tom Esparon <tespo@btinternet.com>
2023-03-07 18:08:55 +0000

    Added alert using rflutter module

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
+// Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 QuizBrain quizBrain = QuizBrain();
 
@@ -33,28 +35,85 @@ class _QuizPageState extends State<QuizPage> {
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
     setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
-      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
-      //HINT! Step 4 Part B is in the quiz_brain.dart
-      //TODO: Step 4 Part C - reset the questionNumber,
-      //TODO: Step 4 Part D - empty out the scoreKeeper.
+      // Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
+      // Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      // Step 4 Part B is in the quiz_brain.dart
+      // Step 4 Part C - reset the questionNumber,
+      // Step 4 Part D - empty out the scoreKeeper.
+      // Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
 
-      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      // Idea: Use API for questions
+      // https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean
+      // or https://the-trivia-api.com/
 
-      if (userPickedAnswer == correctAnswer) {
-        print('user got it right!');
-        scoreKeeper.add(Icon(
-          Icons.check,
-          color: Colors.green,
-        ));
+      if (quizBrain.isFinished() == true) {
+        //This is the code for the basic alert from the docs for rFlutter Alert:
+        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
+
+        // Reusable alert style
+        var alertStyle = AlertStyle(
+            animationType: AnimationType.fromTop,
+            isCloseButton: false,
+            isOverlayTapDismiss: false,
+            descStyle: TextStyle(fontWeight: FontWeight.bold),
+            descTextAlign: TextAlign.center,
+            animationDuration: Duration(milliseconds: 400),
+            alertBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0.0),
+              side: BorderSide(
+                color: Colors.black,
+              ),
+            ),
+            titleStyle: TextStyle(
+              color: Colors.green,
+            ),
+            constraints: BoxConstraints.expand(width: 300),
+            //First to chars "55" represents transparency of color
+            overlayColor: Color(0x55000000),
+            alertElevation: 0,
+            alertAlignment: Alignment.topCenter);
+
+        // Alert dialog using custom alert style
+        Alert(
+          context: context,
+          style: alertStyle,
+          type: AlertType.success,
+          title: "Finished!",
+          desc: "You\'ve reached the end of the quiz. Well Done :)",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "RESTART!",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              color: Colors.blue,
+              radius: BorderRadius.circular(2.0),
+            ),
+          ],
+        ).show();
+
+        //Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+
+        //Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
       } else {
-        print('user got it wrong');
-        scoreKeeper.add(Icon(
-          Icons.close,
-          color: Colors.red,
-        ));
+        if (userPickedAnswer == correctAnswer) {
+          print('user got it right!');
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          print('user got it wrong');
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
       }
-      quizBrain.nextQuestion();
     });
   }
 
